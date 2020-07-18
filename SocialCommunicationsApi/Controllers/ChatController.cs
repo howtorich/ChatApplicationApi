6be97c50d019bb4b170deb7d3ddadfdf48c1666c
Satyanarayana.Daddala <Satyanarayana.Daddala@adaptamed.org>
2020-07-18 19:17:52 +0530

    Added Sample Chat Hub for testing

---
+++ SocialCommunicationsApi/Controllers/ChatController.cs
@@ -0,0 +1,17 @@
+﻿namespace SocialCommunicationsApi.Controllers
+{
+    using Microsoft.AspNet.SignalR.Hubs;
+    using Microsoft.AspNetCore.SignalR;
+
+    [HubName("chatHub")]
+    public class ChatController : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.Client(Context.ConnectionId).SendAsync("Receive", name, message).Wait();
+
+            //Clients.User("").SendAsync("", "");
+        }
+    }
+}
--- SocialCommunicationsApi/Startup.cs
+++ SocialCommunicationsApi/Startup.cs
@@ -4,6 +4,7 @@ namespace SocialCommunicationsApi
     using Microsoft.AspNetCore.Hosting;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.Extensions.Hosting;
+    using SocialCommunicationsApi.Controllers;
 
     public class Startup
     {
@@ -17,6 +18,7 @@ namespace SocialCommunicationsApi
             services.AddControllers();
             // Added By Pavan.Somineni Due to error(Unknown error 0) while Browsing(Rest Call in Chrome) in Chrome.
             services.AddCors();
+            services.AddSignalR();
             services.AddMvc().AddNewtonsoftJson();
         }
 
@@ -41,54 +43,16 @@ namespace SocialCommunicationsApi
             // Added By Pavan.Somineni Due to error(Unknown error 0) while Browsing(Rest Call in Chrome) in Chrome.
             app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
 
+            app.UseEndpoints(endPoint =>
+            {
+                endPoint.MapHub<ChatController>("/chatHub");
+            });
+
             // app.UseAuthorization();
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
         }
-
-
-
-
-
-
-
-
-
-
-
-        //public Startup(IConfiguration configuration)
-        //{
-        //    Configuration = configuration;
-        //}
-
-        //public IConfiguration Configuration { get; }
-
-        //// This method gets called by the runtime. Use this method to add services to the container.
-        //public void ConfigureServices(IServiceCollection services)
-        //{
-        //    services.AddControllers();
-        //}
-
-        //// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        //public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        //{
-        //    if (env.IsDevelopment())
-        //    {
-        //        app.UseDeveloperExceptionPage();
-        //    }
-
-        //    app.UseHttpsRedirection();
-
-        //    app.UseRouting();
-
-        //    app.UseAuthorization();
-
-        //    app.UseEndpoints(endpoints =>
-        //    {
-        //        endpoints.MapControllers();
-        //    });
-        //}
     }
 }
